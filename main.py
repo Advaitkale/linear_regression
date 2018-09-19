dbd46788132171341b262f764dca4bb502f12cb2
Advait Kale <34103551+Advaitkale@users.noreply.github.com>
2018-09-19 07:48:15 +0530

    This is a Machine Learning Algorithm

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-# Import all the dependencies
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 from sklearn import linear_model
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
 # Read data
-data = pd.read_fwf('brain_body.txt')
+data = pd.read_fwf('BrainBody.txt')
 x_values = data[['Brain']]
 y_values = data[['Body']]
 
-# Train model using Linear Regression
-body_prediction = linear_model.LinearRegression()
+# Train model using Decision Tree Regression and Linear Regression
+
+body_prediction = DecisionTreeRegressor(max_depth=100)# Denotes the tree depth
+body_prediction2=linear_model.LinearRegression()
 body_prediction.fit(x_values, y_values)
+body_prediction2.fit(x_values, y_values)
+y_predict=body_prediction.predict(x_values)
+y_predict2=body_prediction2.predict(x_values)
 
 # Visualize results
-plot.scatter(x_values, y_values)
-plot.plot(x_values, body_prediction.predict(x_values))
-plot.show()
+
+plt.plot(x_values, y_predict)
+
+#Calculate mean squared error using sklearn.metrics
+print(mean_squared_error(y_values,y_predict))
+print(mean_squared_error(y_values,y_predict2))
+
